8157f835eb8362b26aecf851768ae1ae6542ec22
John Barnes <ohenesetwumasi@gmail.com>
2024-09-01 10:09:45 +0000

    feat: schedule reminders

--- components/Core/ReviewList.tsx
+++ components/Core/ReviewList.tsx
@@ -1,23 +1,42 @@
 import { View, Text, TouchableOpacity, FlatList, Image } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { sampleMedicines } from "@/constants/data";
-
+import * as SecureStore from "expo-secure-store";
 const ReviewList = () => {
-  const renderItem = ({ item }: { item: (typeof sampleMedicines)[number] }) => (
+  const [drugs, setDrugs] = React.useState([]);
+
+  const fetchDrugs = async () => {
+    const drugs = (await SecureStore.getItemAsync("drugs")) || "[]";
+    setDrugs(JSON.parse(drugs));
+  };
+  useEffect(() => {
+    (async () => await fetchDrugs())();
+  }, []);
+  const renderItem = ({
+    item,
+  }: {
+    item: { name: string; dosage: string; reminders: [string] };
+  }) => (
     <View className="flex-row space-x-5 items-center p-3 px-8  bg-neutral rounded-3xl ">
       <Image source={require("@/assets/images/drugs.png")} />
       <View className="flex-1">
-        <Text className="text-base">{item.name}</Text>
-        <View className="flex-row space-x-2">
-          <Text className="text-gray-600">{item.time}</Text>
-          <Text className={`${item.taken ? "text-green-500" : "text-red-500"}`}>
-            {item.taken ? "Taken" : "Not Taken"}
+        <Text className="text-base">{item?.name}</Text>
+        <View className="flex-row items-center space-x-2">
+          <Text className="text-gray-500">{item?.dosage}</Text>
+          <Text className="text-sm text-gray-500">
+            {item?.reminders?.length} reminders
           </Text>
         </View>
       </View>
       <TouchableOpacity
-        onPress={() => alert(`Viewing details for ${item.name}`)}
+        onPress={() =>
+          alert(
+            `Name: ${item.name}, Dosage: ${
+              item.dosage
+            }, Reminders: ${item.reminders.map((item) => item.time)}`
+          )
+        }
       >
         <AntDesign name="right" size={18} color="gray" />
       </TouchableOpacity>
@@ -29,10 +48,10 @@ const ReviewList = () => {
       <Text className="font-semibold text-lg ">Daily Review</Text>
 
       <FlatList
-        data={sampleMedicines}
+        data={drugs}
         ItemSeparatorComponent={() => <View className="h-4"></View>}
         renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item, index) => index.toString()}
       />
     </View>
   );
